a80260ca386cf4a2cfa1341d5646c6d2117b9f81
Stepuro Mikhail <s1t2e3p4@mail.ru>
2023-02-10 15:36:37 +0300

    Записал функции для просмотра и поиска контакта.

--- user_interface.py
+++ user_interface.py
@@ -1,7 +1,7 @@
 import logger as log
 import data_contact as dc
 import data_display as dd
-import check_input as ch
+import defs_check_input as dci
 
 
 def add_contact():
@@ -19,12 +19,12 @@ def add_contact():
 def import_contact():
     log.log_input_data('Contact import from file requested by user')
     print('Enter a path to file: ')
-    name_file = ch.check_filepath()
+    name_file = dci.check_filepath()
     dc.import_contact_from_file(name_file)
 
 
 def show_contact():
     log.log_input_data('Info about contact requested by user')
     print('Enter a surname: ')
-    surname = ch.check_text()
+    surname = dci.check_text()
     dd.find_contact_in_phonebook(surname)
